7058c94a0a717f1918bc9257b65e30cff4536c69
darrion <dbanks35@gatech.edu>
2019-05-05 21:27:40 -0400

    Dissimilarity/affinity, cloning, and maturation algorithms coded.

--- main.py
+++ main.py
@@ -1,67 +1,36 @@
 import numpy as np
 
-import random
-from collections import deque
+class ImmuneNetwork:
 
-def affinity(ag,Ab):
-    
-    result = np.array([np.linalg.norm(ag-ab) for ab in Ab])
-    return result 
+    def __init__(self):
+        return None 
 
-def select(f, Ab, n):
-    fAb = sorted([(f,Ab) for f,Ab in zip(f, Ab)], key=lambda k:k[0], reverse=True)
-    Abn = [ab[1] for ab in fAb]
-    return Abn[:n]
+    """
+    PrimeDetectors, DetectionRatesYY, DetectionRatesXY, clone_rate, affinity_rate, deletion_rate, suppression_rate
+    """
+    def fit(self, Data, Detectors, n, maximums, learning_rate):
 
-def clone(Abn, B, f):
-    clones = []
-    for i in range(0,len(Abn)):
-        Nc = round((B*len(Abn))/(i+1))
-        clones += ([Abn[i]]*Nc)
-    return clones
+        for i in range(len(Data)):
 
-def hypermutate(C, f):
+            X = [Data[i]]*len(Detectors)
+            X = np.array(X)
+            Y = Detectors 
+            DRXY = [np.linalg.norm(x-y) for x,y in zip(X,Y)]
+            DRXY = np.array(DRXY)
+            DRXY_ix = DRXY.argsort()[-1*maximums:][::-1]
+            DRXY = DRXY[DRXY_ix]
+            DRXY = DRXY/DRXY.sum()
+            DRXY_clones = DRXY*n 
+            DRXY_clones = DRXY_clones.astype(int)
+            Y_clones = Y[DRXY_ix]
+            Y_clones = [[Y_clones[ix]]*DRXY_clones[ix] for ix in DRXY_ix]
+            Y_clones = np.array(Y_clones)
+            Y = Y - learning_rate * (Y[DRXY_ix]-X)
+            DRXY = [np.linalg.norm(x-y) for x,y in zip(X,Y)]
+            DRXY = np.array(DRXY)
 
-    for i in range(len(C)):
-        shift = random.randint(0,len(C[0]))
-        C[i] = np.roll(C[i], shift)
-    return C
 
-def insert(ag, ab_alpha, ab_beta): 
-    a,b = [affinity(ag, ab_alpha), affinity(ag, ab_beta)]
-    return max(a,b)
-
-def clonalg(Ab, Ag, gen, n, d, L, B, M, N):
-    f = []
-    Abm = []
-    C = []
-
-    for i in range(0,gen): 
-        for j in range(0,M):
-            f.append(affinity(Ag[j], Ab))
-            Abn = select(f[j], Ab, n)
-            C += clone(Abn, B, f[j])
-            f_prime = affinity(Ag[j], C)
-            C_prime = hypermutate(C, f_prime)
-            f_prime = affinity(Ag[j], C_prime)
-            ab = select(f_prime, C_prime, 1)
-            Abm.append(ab if (len(Abm) < j+1) else insert(Ag[j], ab, Abm[j]))
-        print(i)
-M=50
-N=10
-n=5
-d=1
-L=5
-B=1
-gen=5
-clonalg(
-    Ab=np.random.randint(0,2,(N,L)),
-    Ag=np.random.randint(0,2,(M,L)),
-    gen=gen,
-    n=n,
-    d=d,
-    L=L,
-    B=B,
-    M=M,
-    N=N
-)
+# delta = number of highest affinity cells. 
+# N = total number of clones.
+im = ImmuneNetwork()
+im.fit(np.random.randint(10,size=(1,5)), np.random.randint(10,size=(10,5)), n=100, maximums=10, learning_rate=0.2)
